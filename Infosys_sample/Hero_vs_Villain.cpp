bb5de0cefd2e9212df1127b7cc9f48f217c4a757
Biswajit Deb <biswa2015.bd@gmail.com>
2024-01-10 19:20:14 +0530

    Hero_VS_Villain_problem_solved

--- Infosys_sample/Hero_vs_Villain.cpp
+++ Infosys_sample/Hero_vs_Villain.cpp
@@ -1,28 +1,23 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int victory(vector<int> Scores)
 {
-    int remove=0;
-    if(Scores[0]>Scores[1])
-        remove=0;
-    else if (Scores[0]<Scores[1])
-        remove=Scores[1]-Scores[0];
-    return remove;
+    return max(Scores[0], Scores[1]);
 }
 
-int win_counter(int V,vector<int> V_health ,int H)
+int win_counter(int V, vector<int> V_health, int H)
 {
     vector<int> Scores;
-    int H_win=0;
-    int V_win=0;
-    for(int i=0;i<V;i++)
+    int H_win = 0;
+    int V_win = 0;
+    for (int i = 0; i < V; i++)
     {
-        if(V_health[i]<H)
+        if (V_health[i] < H)
             H_win++;
-        else if(V_health[i]>H)
+        else if (V_health[i] > H)
             V_win++;
         else
             continue;
@@ -31,32 +26,32 @@ int win_counter(int V,vector<int> V_health ,int H)
     Scores.push_back(H_win);
     Scores.push_back(V_win);
 
-    victory(Scores);
+    return victory(Scores);
 }
-
 
 int main()
 {
-    //No of Heroes
-    int H;
-    cin>>H;
-    //No of Villains
+    // No of Villains
     int V;
-    cin>>V;
+    cin >> V;
 
-    //Health of Heroes
-    int H_health;
-    cin>>H_health;
+    // No of Heroes
+    int H;
+    cin >> H;
 
-    //Health of Villains
+    // Health of Villains
     vector<int> V_health(V);
-    for(int i=0;i<V;i++)
+    for (int i = 0; i < V; i++)
     {
-        cin>>V_health[i];
+        cin >> V_health[i];
     }
 
-    int remove = win_counter(V,V_health,H);
-    cout<<"The minimum number of villains to be removed : "<<remove;
-    //int max = *max_element(V_health.begin(), V_health.end());
+    // Health of Heroes
+    int H_health;
+    cin >> H_health;
+
+    int remove = win_counter(V, V_health, H_health);
+    cout << "The minimum number of villains to be removed : " << remove;
+
     return 0;
 }
--- Infosys_sample/test.cpp
+++ Infosys_sample/test.cpp
@@ -1,17 +1,57 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+int victory(vector<int> Scores)
+{
+    return max(Scores[0], Scores[1]);
+}
+
+int win_counter(int V, vector<int> V_health, int H)
+{
+    vector<int> Scores;
+    int H_win = 0;
+    int V_win = 0;
+    for (int i = 0; i < V; i++)
+    {
+        if (V_health[i] < H)
+            H_win++;
+        else if (V_health[i] > H)
+            V_win++;
+        else
+            continue;
+    }
+
+    Scores.push_back(H_win);
+    Scores.push_back(V_win);
+
+    return victory(Scores);
+}
 
 int main()
 {
-    vector<int> v;
-    int a=10;
-    int b=11;
-    v.push_back(a);
-    v.push_back(b); 
+    // No of Villains
+    int V;
+    cin >> V;
 
-    cout<<v[0];
+    // No of Heroes
+    int H;
+    cin >> H;
+
+    // Health of Villains
+    vector<int> V_health(V);
+    for (int i = 0; i < V; i++)
+    {
+        cin >> V_health[i];
+    }
+
+    // Health of Heroes
+    int H_health;
+    cin >> H_health;
+
+    int remove = win_counter(V, V_health, H_health);
+    cout << "The minimum number of villains to be removed : " << remove;
 
     return 0;
 }
